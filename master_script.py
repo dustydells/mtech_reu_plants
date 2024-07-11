7cee16c22c2e8e984812e33c005d2803e132f5c1
dustydells <98501262+dustydells@users.noreply.github.com>
2024-07-11 15:32:56 -0600

    first draft of master script

--- master_script.py
+++ master_script.py
@@ -1,14 +1,22 @@
 import cv2
 from matplotlib import pyplot as plt
-from functions import crop_to_square
+from functions import crop_to_square, apply_vegetative_index, calc_live_plants_percentage
 
 # Enter path to folder that contains quadrat images here
 path = 'raw_photos\\20240531_103541.jpg'
 
-# CROP IMAGES
-cropped_img = crop_to_square(path)
+# Read in the file in RGB with pyplot (must be in RGB for quadrat crop to work)
+img = plt.imread(path)
 
-cv2.imshow('cropped image', cropped_img)
+# CROP TO QUADRAT SQUARE
+cropped_img = crop_to_square(img)
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# DENOISE IMAGE
+# denoised_img = cv2.bilateralFilter(cropped_img, d=9, sigmaColor=75, sigmaSpace=75)
+
+# APPLY VEGETATIVE INDEX
+rgbvi_img = apply_vegetative_index(cropped_img, 'rgbvi')
+
+# WRITE VI DATA INTO A CSV
+# Calculate a percentage of pixels that are green according to an rgbvi threshold of 130
+percent_green_pixels = calc_live_plants_percentage(vi_img=rgbvi_img, green_threshold=130)
