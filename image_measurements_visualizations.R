81b8c508dfff1e23c67056ca9ffa129c0aefbe00
dustydells <98501262+dustydells@users.noreply.github.com>
2024-07-12 14:39:38 -0600

    made plot of percent rocks >< 5.08cm

--- image_measurements_visualizations.R
+++ image_measurements_visualizations.R
@@ -16,11 +16,31 @@ dat <- read_csv('image_measurements.csv') %>%
 ggplot() +
   geom_beeswarm(dat, mapping = aes(x = as.character(1), y = MajorAxisLength, color = object_size_type_major_axis))
 
+total_rows <- nrow(dat)
+
 # Calculate the percentage of objects that meet >5.08cm criteria for each metric
-percent_summary <- dat %>% 
-  pivot_longer(cols = object_size_type_diameter:object_size_type_major_axis, 
-               names_to = 'metric', 
-               values_to = 'value' ) %>% 
-  group_by(metric) %>% 
-  summarise(percent_greater = aslkdjflsdfkjl / n())
-  # mutate(object_size_type_diameter = subset('>5.08cm') / n())
+percent_summary <- dat %>%
+  pivot_longer(cols = object_size_type_diameter:object_size_type_major_axis,
+               names_to = 'metric',
+               values_to = 'value' ) %>%
+  group_by(metric, value) %>%
+  summarise(percent_greater = n() / total_rows) %>% 
+  # Rename the values in metric
+  mutate(metric = case_when(
+    metric == 'object_size_type_area' ~ 'Area',
+    metric == 'object_size_type_diameter' ~ 'Diameter',
+    metric == 'object_size_type_major_axis' ~ 'Major Axis'
+  ))
+
+# Visualize that summary
+ggplot() +
+  geom_col(percent_summary, mapping = aes(x = metric, y = percent_greater, fill = value)) +
+  coord_flip() +
+  scale_fill_manual('olivedrab', 'ivory') +
+  labs(
+    title = 'Percent of segmented objects classified as greater or less than 5.08cm',
+    fill = 'Object Classification',
+    x = 'Metric by which object was classified',
+    y = 'Percent of objects classified'
+  ) +
+  theme_classic()
