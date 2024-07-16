5239c03429a741677ef4e51e420375716eb11237
dustydells <98501262+dustydells@users.noreply.github.com>
2024-07-16 11:58:37 -0600

    converted this script into function style too

--- master_script_greenhouse.py
+++ master_script_greenhouse.py
@@ -1,24 +1,48 @@
 '''
 This script creates a g r  i d yippee
 It works best on images of plant leaves and stems on a plain background.
+Specify your parameters in the main function.
 '''
 
-from email.mime import application
 import cv2
-
 from functions import apply_vegetative_index
 
-# Enter path to image of plants on a blank background here
-path = 'test_photos_greenhouse/image_10.jpg'
+def main():
+    '''
+    Determine your paramteters here. 
+    '''
+    
+    # Enter path to image of plants on a blank background here
+    path = 'test_photos_greenhouse/image_10.jpg'
 
-# Enter output path and filename here.
-output_path = 'results/master_script_output/image_10_greenhouse_grid.jpg'
+    # Enter output path and filename here.
+    output_path = 'results/master_script_output/image_10_greenhouse_grid.jpg'
 
-# Read in the image in color
-img = cv2.imread(path, 0)
+    # Determine vegetative index that will be used
+    index_type = 'exg' # ExG (Excess Green) worked well on greenhouse photos, which are of willow leaves and stems on a gray background.
 
-# APPLY VEGETATIVE INDEX
-# Determine vegetative index that will be used
-index_type = 'exg'
+    run_script(path, output_path, index_type)
 
-vi_img = apply_vegetative_index(img, index_type)
+
+def run_script(path, output_path, index_type):
+    '''
+    Run the entire process with the parameters you specified.
+
+    Parameters:
+        path:
+            String. Path to the input image
+        output_path:
+            String. Path of the output image that will be saved
+        index_type:
+            String. The Vegetative index you'd like to use. 
+            Options: exg, exr, grvi, vari, rgbvi, exg-exr
+    '''
+    
+    # Read in the image in color
+    img = cv2.imread(path, 0)
+
+    # APPLY VEGETATIVE INDEX
+    vi_img = apply_vegetative_index(img, index_type)
+
+
+main()
