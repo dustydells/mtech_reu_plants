162ff91548b799bcf37f0fd957cfe8270e33efe9
dustydells <98501262+dustydells@users.noreply.github.com>
2024-07-17 17:10:49 -0600

    one iteration of loop worked all right

--- master_script_on_multiple_images.py
+++ master_script_on_multiple_images.py
@@ -5,5 +5,13 @@ from master_script import run_script
 # Enter path to folder that contains all photos that will be sent through the loop
 folder = 'raw_photos\\*'
 
-for file in glob.glob(folder):
-    print('function has run for', file)
+# Determine parameters
+index_type = 'rgbvi'
+green_threshold = 130
+
+for i, path in enumerate(glob.glob(folder)):
+    output_path = f'results/master_script_output/all_photos/grid_{i}.jpg'
+
+    run_script(path, output_path, index_type, green_threshold, crop=True)
+
+    print('function has run for', path)
