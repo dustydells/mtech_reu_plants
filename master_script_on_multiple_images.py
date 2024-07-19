940fc0e6ca3e730e977b82fb01c7db78ff4d5143
dustydells <98501262+dustydells@users.noreply.github.com>
2024-07-19 15:46:18 -0600

    fixed mismatched files issue

--- master_script_on_multiple_images.py
+++ master_script_on_multiple_images.py
@@ -1,57 +1,57 @@
 import glob
 from matplotlib import pyplot as plt
+from functions import reorder_file_paths
 from master_script import run_script
 
 
 # Enter path to folder that contains all photos that will be sent through the loop
 folder = 'raw_photos_cropped\\*'
-files = []
-# Get a list of all the files in that folder 
-for file in glob.glob(folder):
-    files.append(file)
+# Get a list of all the files in that folder in order of number
+files = reorder_file_paths(folder)
+
 
 
 # If your images have already been cropped and save, enter a folder here that contains the original photos before they were cropped. 
 run_w_already_cropped = True
 # Save the filenames in a list
 if run_w_already_cropped == True:
-    raw_files = []
-    raw_imgs_folder = 'raw_photos\\*' # Put the path to the raw images here
-    for raw_file in glob.glob(raw_imgs_folder):
-        raw_files.append(raw_file)
-
-
-# Determine parameters
-index_type = 'rgbvi'
-green_threshold = 135
-crop = False
-keep_cropped_image = False
+    raw_imgs_folder = 'raw_photos_numbered\\*' # Put the path to the raw images here
+    raw_files = reorder_file_paths(raw_imgs_folder)
 
 for i in range(len(files)):
-    # Skip the ones I've already done
-    # if i <= 57: # suffering
-    #     continue
-    # else:
-        path = files[i]
-        output_path = f'results/master_script_output/all_photos/grid_{i}.jpg'
+    print(files[i], raw_files[i])
 
-        # if the images are getting cropped, save it to file
-        if crop == True: 
-            cropped_img = run_script(path, output_path, index_type, green_threshold, crop)
+# # Determine parameters
+# index_type = 'rgbvi'
+# green_threshold = 135
+# crop = False
+# keep_cropped_image = False
 
-            # If you want to save the cropped images in a folder 
-            if keep_cropped_image == True:
-                # Save the cropped images so you never have to go through that process again
-                plt.imsave(f'raw_photos_cropped/image_{i}.jpg', cropped_img)
+# for i in range(len(files)):
+#     # Skip the ones I've already done
+#     # if i <= 57: # suffering
+#     #     continue
+#     # else:
+#         path = files[i]
+#         output_path = f'results/master_script_output/all_photos/grid_{i}.jpg'
 
-        # If the files have already been cropped and saved in a folder
-        elif run_w_already_cropped == True:
-            raw_imgs_path = raw_files[i]
-            run_script(path, output_path, index_type, green_threshold, crop, raw_imgs_path)
+#         # if the images are getting cropped, save it to file
+#         if crop == True: 
+#             cropped_img = run_script(path, output_path, index_type, green_threshold, crop)
 
-        # Just run the function like normal!!
-        else:
-            run_script(path, output_path, index_type, green_threshold, crop)
+#             # If you want to save the cropped images in a folder 
+#             if keep_cropped_image == True:
+#                 # Save the cropped images so you never have to go through that process again
+#                 plt.imsave(f'raw_photos_cropped/image_{i}.jpg', cropped_img)
 
-        print(f'function has run for image {i}: {path}')
+#         # If the files have already been cropped and saved in a folder
+#         elif run_w_already_cropped == True:
+#             raw_imgs_path = raw_files[i]
+#             run_script(path, output_path, index_type, green_threshold, crop, raw_imgs_path)
+
+#         # Just run the function like normal!!
+#         else:
+#             run_script(path, output_path, index_type, green_threshold, crop)
+
+#         print(f'function has run for image {i}: {path}')
 
