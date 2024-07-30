2f5e768b86a82e2e2360ff2b2c6eed8d57fc014b
dustydells <98501262+dustydells@users.noreply.github.com>
2024-07-30 13:17:47 -0600

    ran a test, still works post bug fix

--- master_script.py
+++ master_script.py
@@ -16,13 +16,13 @@ def main():
     '''
 
     # Enter parameters.
-    path = 'photos/test_photos_greenhouse/image_10.jpg' # path to input image
-    output_path = 'results/master_script_output/example_greenhouse.jpg' # output path and filename. This is where your result will be saved. 
+    path = 'photos/raw_photos_cropped/image_10.jpg' # path to input image
+    output_path = 'results/master_script_output/test.jpg' # output path and filename. This is where your result will be saved. 
     crop = False # whether your photos need to be cropped into a square or not.
     keep_cropped_image = False # If your photos get cropped, choose here whether you want the cropped image to be saved. Definitely do this if you have to crop a lot of photos at once in a for loop or something. It takes forever and you should only have to do it once. 
 
     # Determine vegetation index that will be used
-    index_type = 'exg' # RGBVI worked well for my quadrat photos, ExG worked well on the willow leaf photos - feel free to experiment
+    index_type = 'rgbvi' # RGBVI worked well for my quadrat photos, ExG worked well on the willow leaf photos - feel free to experiment
 
     # INVESTIGATE HISTOGRAM
     '''
@@ -35,12 +35,12 @@ def main():
 
     # Determine threshold that will differentiate between live and dead plants.
     # Or experiment with skimage filters like otsu and median.
-    green_threshold = 155 # 135 worked well on RGBVI photos. 155 worked well on ExG photos. 
+    green_threshold = 135 # 135 worked well on RGBVI photos. 155 worked well on ExG photos. 
 
 
     # Run the process
     if crop == False:
-        run_script(path, output_path, index_type, green_threshold, crop)
+        run_script(path, output_path, index_type, green_threshold, crop, raw_imgs_path='photos/raw_photos_numbered/image_10.jpg')
 
     elif crop == True: # Run the process, but crop the images to a square first. 
         cropped_img = run_script(path, output_path, index_type, green_threshold, crop)
